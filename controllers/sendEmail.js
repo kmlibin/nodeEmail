9ca377a16f86370f42519e5050a39f8e1e2d4b20
Kelli Libin <km.putnam2@gmail.com>
2023-06-12 10:54:15 -0600

    example with ethereal and sendgrid

--- controllers/sendEmail.js
+++ controllers/sendEmail.js
@@ -1,5 +1,39 @@
-const sendEmail = async (req, res) => {
-  res.send("hello");
+const nodemailer = require("nodemailer");
+const sgMail = require('@sendgrid/mail')
+
+const sendEmailEthereal = async (req, res) => {
+  let testAccount = await nodemailer.createTestAccount();
+  //connect to our service
+  const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    auth: {
+      user: "susana.mueller@ethereal.email",
+      pass: "PzJdny1PV7nBUT7fCB",
+    },
+  });
+  //send mail
+  let info = await transporter.sendMail({
+    from: 'Test Person <testperson@test.com>',
+    to: 'bar@example.com',
+    subject: 'hello',
+    text: 'hello world with node.js'
+  })
+  res.json(info);
 };
 
-module.exports = sendEmail;
+const sendEmailSendGrid = async (req,res) => {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+        to: 'email', //send to real address 'realaddress@mail.com',
+        from: 'email', //'email of your verified email from sendgrid',
+        subject: 'send grid is fun',
+        text: 'hello text',
+        html: '<strong>html text</strong>'
+    }
+    const info = await sgMail.send(msg);
+    res.json(info);
+}
+
+//see videos 262-264 for sendgrid for production
+module.exports = sendEmailEthereal;
